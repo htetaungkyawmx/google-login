fc18d8222e0fab0e9b507821d97bb292f9c189f7
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-05-02 18:37:18 +0630

    Initial commit

---
+++ src/test/java/org/solt/googlelogin/GoogleLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.solt.googlelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
